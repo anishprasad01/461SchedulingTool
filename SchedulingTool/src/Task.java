4f87494c90329215d446d89bf3a8937591790228
Anish Prasad <anishnp01@outlook.com>
2018-05-05 11:12:06 -0700

    Created Task.java
    Created the file for the Task Object

---
+++ SchedulingTool/src/Task.java
@@ -0,0 +1,22 @@
+
+public class Task extends SchedulingObject {
+	
+	
+
+	public Task(String newName) {
+		super(newName);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Task(SchedulingObject toCopy) {
+		super(toCopy);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected int generateID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
