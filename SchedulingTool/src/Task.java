761fe8a8896ca189b7bc2bff7f6dfd3c9e5f90eb
Anish Prasad <anishnp01@outlook.com>
2018-05-05 17:20:36 -0700

    Added Data Fields in Task
    Added the required data fields to task.java

--- SchedulingTool/src/Task.java
+++ SchedulingTool/src/Task.java
@@ -1,6 +1,15 @@
 
+import java.time.*;
+
 public class Task extends SchedulingObject {
 	
+	private int projectID = 0;
+	private String taskOwner = null;
+	private int previousTaskID = 0;
+	private int nextTaskID = 0;
+	private int taskDuration = 0;
+	private LocalDate startDate = null;
+	private LocalDate endDate = null;
 	
 
 	public Task(String newName) {
