9c6f15917818d3937369e92df4f4782c4bbc4db4
Hossam Basiony <hbasiony@uw.edu>
2018-05-23 18:48:29 -0700

    readded my lost stuff due to the merge conflict

--- SchedulingTool/src/Project.java
+++ SchedulingTool/src/Project.java
@@ -80,4 +80,93 @@ public class Project extends SchedulingObject {
 	}
 
 
+
+	//This method loops through all the tasks and assigns their early start, late start, early finish, and late finish
+
+	public void setupCP(long earlyStartOfFirstActivity, long lateStartOfFirstActivity) {
+
+		for (int i = 0; i < taskList.size(); i++) {
+
+			// if it is the first element
+			if (i == 0) {
+
+				//set early start and late start similar to first argument
+				//for early finish and late finish, they will simply be the early start and late start + duration of task
+
+				taskList.get(i).setEarlyStart(earlyStartOfFirstActivity);
+				taskList.get(i).setLateStart(lateStartOfFirstActivity);
+				taskList.get(i).setEarlyFinish(taskList.get(i).getEarlyStart() + taskList.get(i).getTaskDuration());
+				taskList.get(i).setLateFinish(taskList.get(i).getLateFinish() + taskList.get(i).getTaskDuration());
+			} else {
+
+				//set early start and late start early finish and late finish of the previous task
+				//for early finish and late finish, they will simply be the early start and late start + duration of task
+
+				taskList.get(i).setEarlyStart(taskList.get(i - 1).getEarlyFinish());
+				taskList.get(i).setLateStart(taskList.get(i - 1).getLateFinish());
+				taskList.get(i).setEarlyFinish(taskList.get(i).getEarlyStart() + taskList.get(i).getTaskDuration());
+				taskList.get(i).setLateFinish(taskList.get(i).getLateFinish() + taskList.get(i).getTaskDuration());
+
+
+			}
+
+		}
+
+	}
+
+	//A method that returns the index of a task whose name is specified as input
+	//so that it can be obtained from the task list using that index.
+	//it is a helper for findTaskByName() and it returns -1 if fails.
+	public int getTaskIndexByName(String input) {
+		for (int i = 0; i < taskList.size(); i++) {
+			if (taskList.get(i).getName() == input) {
+
+				return i;
+			}
+
+		}
+		return -1;
+	}
+
+	//Gets free float of two activities whose names are entered as input string
+	//First argument is current activity, and second argument is next activity
+	//it returns -1 if either of the activities has the incorrect name passed in
+	public long getFreeFloat(String currentActivity, String NextActivity) {
+
+		if (getTaskIndexByName(currentActivity) == -1 || getTaskIndexByName(NextActivity) == -1) {
+			//if either activity is not found
+
+
+			return -1;
+		} else {
+
+			//return earlystart of current activity - early finish of next activity
+
+			return (taskList.get(getTaskIndexByName(NextActivity)).getEarlyStart() - taskList.get(getTaskIndexByName(currentActivity)).getEarlyFinish());
+
+		}
+
+
+	}
+
+	//Gets total float of current activity whose name is entered as input string
+	//it returns -1 if either of the activities has the incorrect name passed in
+
+	public long getTotalFloat(String currentActivity){
+
+		if(getTaskIndexByName(currentActivity) == -1){
+
+			//if activity is not found by name
+
+			return -1;
+		}
+		else{
+			//return late finish - early finish of this activity
+
+			return (taskList.get(getTaskIndexByName(currentActivity)).getLateFinish() - taskList.get(getTaskIndexByName(currentActivity)).getEarlyFinish());
+
+		}
+	}
+
+
 }
