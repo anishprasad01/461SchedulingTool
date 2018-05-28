817b538561d14c02d721f0efe9131db9098a8fdc
Hossam Basiony <hbasiony@uw.edu>
2018-05-27 17:32:48 -0700

    Implemented more tests for the project.java

--- SchedulingTool/src/Test.java
+++ SchedulingTool/src/Test.java
@@ -16,6 +16,11 @@ public class Test {
 		testProject2.setName("TestProject2");
 		testProject2.addTask(new Task("Code the program", "Rahil", LocalDate.of(2018, 5, 19),
                 LocalDate.of(2018, 5, 30), 0, 2));
+
+		testProject2.addTask(new Task("Test The Program", "Hossam", LocalDate.of(2018, 5, 27),
+				LocalDate.of(2018, 5, 30), 0, 2));
+
+
 		System.out.println(testProject2.toString());
 
 		//Test that setup early start, early finish, etc
@@ -27,7 +32,7 @@ public class Test {
         	System.out.println("Could not find specified task");
 		}
 		else{
-        	tempTask.setTaskDuration(5);
+        	tempTask.calculateDuration();
 			testProject2.setupCP(1,3);
 			System.out.println("Found task and it will be on: " + tempTask.getStartDate());
         	System.out.println("It has early Start: " + tempTask.getEarlyStart());
@@ -36,10 +41,21 @@ public class Test {
         	System.out.println("It has early finish: "+ tempTask.getEarlyFinish());
         	System.out.println("It has late finish: " + tempTask.getLateFinish());
         	System.out.println("Total float: " + testProject2.getTotalFloat(tempTask.getName()) );
+            System.out.println("Task Owner: " + tempTask.getTaskOwner());
+            System.out.println("Parent Project Owner ID: " + tempTask.getParentProjectID());
+            System.out.println("Previous Task ID: " + tempTask.getPreviousTaskID());
+            System.out.println("Next Task ID: "+ tempTask.getNextTaskID());
+            System.out.println("Start Date: "+ tempTask.getStartDate());
+            System.out.println("End Date: "+ tempTask.getEndDate());
+
 
 		}
 
+		//testProject2.printAllTaskNames();
 
+		System.out.println(testProject2.getManagerName());
+		System.out.println(testProject2.getProjectName());
+		System.out.println(testProject2.getID());
 
 
 	}
